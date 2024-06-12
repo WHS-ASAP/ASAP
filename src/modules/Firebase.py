c5e6b5d14015e8f536b927ebb7612b604aa87a7b
jeongahn <inetty@naver.com>
2024-06-12 17:50:24 +0900

    [Feat] 각 브랜치 별 코드 파일 modules에 생성 #9

--- src/modules/Firebase.py
+++ src/modules/Firebase.py
@@ -1,22 +1,5 @@
 import re
 
-# From AVASTA -> 이건 연구해서 다시 해볼 필요성 있음
-class ApiKeyAnalyzer:
-    def __init__(self):
-        self.pattern = re.compile(r"['\"]api[_-]?key['\"]\s*:\s*[\"']?[a-zA-Z0-9]+[\"']?")
-
-    def run(self, file_content):
-        matches = self.pattern.findall(file_content)
-        return matches if matches else None
-
-class PasswordAnalyzer:
-    def __init__(self):
-        self.pattern = re.compile(r"['\"]password['\"]\s*[\"']?[a-zA-Z0-9]+[\"']?")
-
-    def run(self, file_content):
-        matches = self.pattern.findall(file_content)
-        return matches if matches else None
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class FirebaseDatabaseAnalyzer:
     def __init__(self):
         self.pattern = re.compile(r'<string name="firebase_database_url">https://[a-zA-Z0-9.-]+\.firebaseio\.com</string>')
