ba6c1b46e636b7515ae37f340abc3a1203d5203a
HANSOWON\이윤성 <bestdldbstjd@gmail.com>
2024-06-22 15:25:05 +0900

    Feat-DeepLink 모듈 구현-#19

--- src/modules/DeepLink.py
+++ src/modules/DeepLink.py
@@ -1,21 +1,69 @@
-import re
+import xml.etree.ElementTree as ET
+from itertools import product
 
 class DeepLinkAnalyzer:
     def __init__(self):
-        self.uri_scheme_pattern = re.compile(r'<data android:scheme="(\w+)"')
-        self.host_pattern = re.compile(r'<data android:host="([^"]+)"')
-        self.intent_filter_pattern = re.compile(r'<intent-filter>(.*?)</intent-filter>', re.DOTALL)
+        self.android_ns = 'http://schemas.android.com/apk/res/android'
 
     def analyze_manifest(self, file_content):
-        uri_schemes = self.uri_scheme_pattern.findall(file_content)
-        hosts = self.host_pattern.findall(file_content)
-        intent_filters = self.intent_filter_pattern.findall(file_content)
-        if not uri_schemes and not hosts and not intent_filters:
-            return None
-        return {"uri_schemes": uri_schemes, "hosts": hosts, "intent_filters": intent_filters}
+        deeplink_results = []
+
+        try:
+            # XML 파싱
+            root = ET.fromstring(file_content)
+        except ET.ParseError as e:
+            print("XML 파싱 에러:", e)
+            return deeplink_results
+
+        for intent_filter in root.findall(".//intent-filter"):
+            schemes = []
+            hosts = []
+            ports = []
+            all_paths = []
+
+            for data in intent_filter.findall("data"):
+                scheme = data.get(f'{{{self.android_ns}}}scheme')
+                host = data.get(f'{{{self.android_ns}}}host')
+                port = data.get(f'{{{self.android_ns}}}port')
+                path = data.get(f'{{{self.android_ns}}}path')
+                path_prefix = data.get(f'{{{self.android_ns}}}pathPrefix')
+                path_pattern = data.get(f'{{{self.android_ns}}}pathPattern')
+                path_advanced_pattern = data.get(f'{{{self.android_ns}}}pathAdvancedPattern')
+                path_suffix = data.get(f'{{{self.android_ns}}}pathSuffix')
+
+                if scheme and scheme not in ["http", "https"]:
+                    schemes.append(scheme)
+                if host:
+                    hosts.append(host)
+                if port:
+                    ports.append(port)
+                if path:
+                    all_paths.append(path)
+                if path_prefix:
+                    all_paths.append(path_prefix)
+                if path_pattern:
+                    all_paths.append(path_pattern)
+                if path_advanced_pattern:
+                    all_paths.append(path_advanced_pattern)
+                if path_suffix:
+                    all_paths.append(path_suffix)
+
+            if not all_paths:
+                all_paths.append('')  # 빈 경로 추가
+
+            for scheme in schemes:
+                for host, port, path in product(hosts or [''], ports or [''], all_paths):
+                    uri = f"{scheme}://{host}"
+                    if port:
+                        uri += f":{port}"
+                    uri += f"{path}"
+                    print("생성된 URI:", uri)  # 디버그 출력
+                    deeplink_results.append(uri)
+
+        return deeplink_results
 
     def run(self, file_content):
-        manifest_results = self.analyze_manifest(file_content)
-        if not manifest_results:
+        deeplink_results = self.analyze_manifest(file_content)
+        if not deeplink_results:
             return None
-        return {"manifest": manifest_results}
+        return {"manifest": deeplink_results}
